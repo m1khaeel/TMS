6f442f3708d0f9fc3d0744be0ffb9f0337cba46f
Mikhail <118671974+m1khaeel@users.noreply.github.com>
2022-11-24 18:58:42 +0300

    Add files via upload

---
+++ Lesson3/Lesson3_dz4.py
@@ -0,0 +1,33 @@
+
+# SPOSOB 1:
+# n = int(input("Введите число: "))
+# b = range(1, n + 1)
+# s = 0
+# for i in b:
+#    s += i**3
+#    print(s)
+
+# SPOSOB 2:
+
+n = int(input("Введите число: "))
+c = -1
+s = 0
+while True:
+    c += 1
+    if c >= n + 1:
+        break
+    s += c**3
+    print(s)
+
+
+# SPOSOB 3)
+
+#n = int(input("Введите число: "))
+#s = (n**2*(n + 1)**2) / 4
+#print(int(s))
+
+
+
+
+
+
