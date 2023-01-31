b5194e7637dbad5d292476e009246e60e36da13a
Mikhail <118671974+m1khaeel@users.noreply.github.com>
2023-01-31 13:15:39 +0300

    Add files via upload

---
+++ Lesson14/Lesson14_dz_bin_poisk.py
@@ -0,0 +1,30 @@
+
+
+from random import randint
+
+my_list = []
+
+for i in range(10):
+    my_list.append(randint(1, 20))
+
+my_list.sort()
+print(my_list)
+
+x = int(input("Введите число для поиска: "))
+
+h_idx = len(my_list) - 1
+l_idx = 0
+m_idx = len(my_list) // 2
+
+while my_list[m_idx] != x and l_idx <= h_idx:
+    if x > my_list[m_idx]:
+        l_idx = m_idx + 1
+    else:
+        h_idx = m_idx - 1
+    m_idx = (l_idx + h_idx) // 2
+
+
+if x == my_list[m_idx]:
+    print(f"Число {x} имеет индекс: {m_idx}")
+else:
+    print("Данного числа в списке нет!!!")
